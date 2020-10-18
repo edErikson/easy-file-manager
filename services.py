b3f2d61dff563482fe92bd6b342fe408f869730d
edErikson <ed.d.erikson@gmail.com>
2020-10-18 20:49:48 +0300

    update

--- services.py
+++ services.py
@@ -29,19 +29,17 @@ def get_media_properties(filename):
 
 def word_count_dict(filename):
     """Returns a word/count dict for this filename."""
-    # Utility used by count() and Topcount().
-    word_count = {}  # Map each word to its count
+    word_count = {}
     input_file = open(filename, 'r', encoding="utf8")
     for line in input_file:
         words = line.split()
         for word in words:
             word = word.lower()
-            # Special case if we're seeing this word for the first time.
             if not word in word_count:
                 word_count[word] = 1
             else:
                 word_count[word] = word_count[word] + 1
-    input_file.close()  # Not strictly required, but good form.
+    input_file.close()
     return word_count
 
 
