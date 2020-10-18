8705f16f3c132ea0e0ed486dfc1b38981c00730f
edErikson <ed.d.erikson@gmail.com>
2020-10-18 21:22:03 +0300

    refactor

--- database.py
+++ database.py
@@ -6,7 +6,6 @@ sql_command_dict = {
     'first_sql': "CREATE TABLE IF NOT EXISTS file_paths(id integer primary key, file_path text not null unique)",
     'add_data_sql': "INSERT OR IGNORE INTO file_paths(file_path) VALUES (?)",
     'table_name_sql': "SELECT name FROM sqlite_master where type='table'",
-    'all_data': "SELECT file_path FROM file_paths",
     }
 
 
@@ -34,6 +33,26 @@ def insert_path_data(data):
     db_connection(sql_command_dict['add_data_sql'], (data,))
 
 
+def db_search(search_str=''):
+    search_sql = "SELECT id, file_path FROM file_paths WHERE file_path LIKE'%%%s%%'" % search_str
+    return db_connection(search_sql, receive=True)
+
+
+def db_delete_record(data):
+    deletion_sql = "DELETE FROM file_paths WHERE id=?"
+    return db_connection(deletion_sql, (data,))
+
+
+def create_table(table_name=''):
+    sql_query = "CREATE TABLE IF NOT EXISTS %s(item_id INTEGER PRIMARY KEY, id INTEGER NOT NULL, " \
+                "name TEXT NOT NULL, size INTEGER, FOREIGN KEY (id) REFERENCES file_paths(id))" % table_name
+    try:
+        db_connection(sql_query)
+        print('Created table : ', table_name)
+    except Exception as e:
+        print('Error on string: ', e)
+
+
 def get_table_data(table_name=''):
     sql_query = "SELECT id, name, size FROM %s" % table_name
     try:
@@ -50,16 +69,6 @@ def insert_data(item_id, name, size, table_name=''):
     db_connection(sql_query, (item_id, name, size,))
 
 
-def create_table(table_name=''):
-    sql_query = "CREATE TABLE IF NOT EXISTS %s(item_id INTEGER PRIMARY KEY, id INTEGER NOT NULL, " \
-                "name TEXT NOT NULL, size INTEGER, FOREIGN KEY (id) REFERENCES file_paths(id))" % table_name
-    try:
-        db_connection(sql_query)
-        print('Created table : ', table_name)
-    except Exception as e:
-        print('Error on string: ', e)
-
-
 def get_item_path(data, table_name=''):
     sql = "SELECT file_path FROM file_paths WHERE id IN (SELECT id FROM %s WHERE id=?);" % table_name
     return db_connection(sql, (data,), receive=True)
@@ -73,16 +82,6 @@ def sorted_name(table_name=''):
 def sorted_size(table_name=''):
     sql_query = "SELECT id, name, size FROM %s ORDER BY size DESC" % table_name
     return db_connection(sql_query, receive=True)
-
-
-def db_search(search_str=''):
-    search_sql = "SELECT id, file_path FROM file_paths WHERE file_path LIKE'%%%s%%'" % search_str
-    return db_connection(search_sql, receive=True)
-
-
-def db_delete_record(data):
-    deletion_sql = "DELETE FROM file_paths WHERE id=?"
-    return db_connection(deletion_sql, (data,))
 
 
 def get_table_names():
